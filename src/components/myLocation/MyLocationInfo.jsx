c1fba1647d1f76281691364a7eaec239b55071c1
sudip <paudelsudip134@gmail.com>
2024-08-04 22:58:24 +0545

    basic styling

--- src/components/myLocation/MyLocationInfo.jsx
+++ src/components/myLocation/MyLocationInfo.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./myLocation.css"
-import { FaSun } from 'react-icons/fa';
+import "./myLocation.css";
+import { FaSun } from "react-icons/fa";
 const MyLocationInfo = () => {
-
   return (
-    <div className="w-[60vw] h-[100vh] flex justify-between flex-col relative text-[#000000] bg-[#b150cc66]">
+    <div className="w-[60vw] h-[100vh] flex justify-between flex-col relative text-[#ffffff] bg-[#669EB7]">
       <section className="w-[60vw] px-[3vw] py-[5vh] flex items-end flex-col">
         <div id="my-location" className="text-5xl">
           Pokhara
@@ -14,22 +13,25 @@ const MyLocationInfo = () => {
         </div>
       </section>
       <section className="w-[60vw] px-[3vw] py-[3vh] flex flex-row justify-between">
-        <div>
-          <div id="time" className="text-3xl">17:18:39</div>
-          <div id="date" className="text-3xl">4 August 2024</div>
+        <div className="text-[#000]">
+          <div id="time" className="text-3xl">
+            17:18:39
+          </div>
+          <div id="date" className="text-3xl">
+            4 August 2024
+          </div>
         </div>
         <div id="temperature" className="text-6xl">
           32<sup>o</sup>C
         </div>
       </section>
-      <section className="flex justify-between flex-col absolute top-[25vh] left-[20vw]">
-        <div className="icon">
-          <FaSun size={300} color="orange" />
-          <div className="text-5xl pl-[7vw]">HAZE</div>
+      <section className="flex justify-between flex-col absolute top-[22vh] left-[20vw]">
+        <div className="icon flex justify-center items-center flex-col">
+          <FaSun size={300} color="#FDCC17" />
+          <div className="text-5xl pt-[3vh] text-[#FDCC17]">HAZE</div>
         </div>
       </section>
-      <p className="absolute top-[10vh] left-[3vw] text-3xl">My Location
-      </p>
+      <p className="absolute top-[10vh] left-[3vw] text-3xl text-[#000]">My Location</p>
     </div>
   );
 };
--- src/components/searchLocation/SearchLocationInfo.jsx
+++ src/components/searchLocation/SearchLocationInfo.jsx
@@ -1,42 +1,54 @@
 import React from "react";
 
-import { FaSun } from "react-icons/fa";
+import { FaSun, FaSearch } from "react-icons/fa";
 const SearchLocationInfo = () => {
   return (
-    <div className="w-[60vw] h-[100vh] flex justify-start items-start flex-col relative text-[#000000] bg-[#00000092]">
-      <div className="search-box">
-        <input type="text" placeholder="search city" />
-        <span>search icon</span>
+    <div className="w-[40vw] h-[100vh] flex justify-start items-start flex-col relative text-[#333] bg-[#ffffff86] px-[3vw] py-[5vh]">
+      <div className="search-box flex flex-row relative">
+        <input
+          type="text"
+          placeholder="search city"
+          className="outline-none px-[25px] py-[2vh] bg-transparent w-[32vw] rounded-l-full text-[#669EB7] border-[#669EB7] border-[2px] text-2xl"
+        />
+        <span className="flex justify-center items-center w-[5vw] h-[10vh] p-[15px] bg-[#669EB7] rounded-full relative bottom-[0px] right-[2vw] cursor-pointer">
+          <FaSearch size={30} color="white" />
+        </span>
       </div>
-      <p>line</p>
-      <div className="icon">
-        <FaSun size={120} color="orange" />
-        <div className="text-xl pl-[3vw]">HAZE</div>
-      </div>
-      <p>line</p>
-      <div className="info">
+      <section className="flex flex-row w-[40vw] my-[5vh] text-[#000]">
+        <div className="icon flex justify-center items-start flex-col pl-[5vw] w-[10vw] text-3xl">
+          <div className="text-3xl">Pokhara</div>
+          <div className="pl-[3vw] text-xl">NEP</div>
+        </div>
+        <div className="icon flex justify-center items-center flex-col w-[25vw]">
+          <FaSun size={120} color="#FDCC17" />
+          <div className="text-xl">HAZE</div>
+        </div>
+      </section>
+      <div className="info text-2xl w-[40vw]">
+        <hr className="bg-[#333] relative right-[3vw] m-[10px]" />
         <div>
           Temperature : 32<sup>o</sup>C (HAZE)
         </div>
-        <div>
+        <div className="text-xl">
           <span>
             High : 32<sup>o</sup>C
           </span>
-          <span>
+          <span className="ml-[5vw]">
             Low : 32<sup>o</sup>C
           </span>
         </div>
-        <p>line</p>
+        <hr className="bg-[#333] relative right-[3vw] m-[10px]" />
         <div>Humidity : 22 g/cc</div>
-        <p>line</p>
+        <hr className="bg-[#333] relative right-[3vw] m-[10px]" />
         <div>Visibility : 22 g/cc</div>
-        <p>line</p>
+        <hr className="bg-[#333] relative right-[3vw] m-[10px]" />
         <div>Wind Speed : 22 g/cc</div>
-        <p>line</p>
+        <hr className="bg-[#333] relative right-[3vw] m-[10px]" />
         <div>
           <span>Sunrise : 5 AM</span>
-          <span>Sunset : 5 PM</span>
+          <span className="ml-[5vw]">Sunset : 5 PM</span>
         </div>
+        <hr className="bg-[#333] relative right-[3vw] m-[10px]" />
       </div>
     </div>
   );
